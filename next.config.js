29b1e41f7b9e016618d1af3038208b2b7252082a
fukata <tatsuya.fukata@gmail.com>
2023-05-29 23:14:46 +0900

    add runtime=edge

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['tsx', 'ts']
+  pageExtensions: ['tsx', 'ts'],
+  experimental: {
+    runtime: 'edge'
+  }
 }
 
 module.exports = nextConfig
